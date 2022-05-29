e3f503aa989092664906a91c899349563fb11e4a
Albert Portnoy <albert@albertp.dev>
2022-05-29 16:49:56 -0500

    Add option to hide button

---
+++ Components/Settings.jsx
@@ -0,0 +1,20 @@
+const {React} = require('powercord/webpack');
+const {SwitchItem} = require('powercord/components/settings');
+
+module.exports = class Settings extends React.PureComponent {
+	render() {
+		const {getSetting, toggleSetting, updateSetting} = this.props;
+
+		return (
+			<div>
+				<SwitchItem
+					note="Show button to insert tone indicators in file upload menu"
+					value={getSetting('showInsertToneBtn', true)}
+					onChange={() => toggleSetting('showInsertToneBtn', true)}
+				>
+					Show Insert Button
+				</SwitchItem>
+			</div>
+		);
+	}
+};
